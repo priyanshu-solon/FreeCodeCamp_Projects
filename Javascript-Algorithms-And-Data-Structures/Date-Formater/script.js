8ebe6ecda138843beefc93ddffe564711d0a804f
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-07-27 22:34:50 +0530

    ok

---
+++ Javascript-Algorithms-And-Data-Structures/Date-Formater/script.js
@@ -0,0 +1,19 @@
+const currentDateParagraph = document.getElementById("current-date");
+const dateOptionsSelectElement = document.getElementById("date-options");
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+    formattedDate=currentDateParagraph
+  }
+});
