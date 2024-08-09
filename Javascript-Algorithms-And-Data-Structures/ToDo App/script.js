a8210135f3a96e56dd7d5f93e1b13f3f7e98357d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-09 22:38:21 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/ToDo App/script.js
+++ Javascript-Algorithms-And-Data-Structures/ToDo App/script.js
@@ -28,13 +28,15 @@ discardBtn.addEventListener("click", () => {
   taskForm.classList.toggle("hidden");
 });
 
+
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
   const taskObj = {
-    id: titleInput.value.toLowerCase()
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
   };
-  console.log(taskObj);
 });
