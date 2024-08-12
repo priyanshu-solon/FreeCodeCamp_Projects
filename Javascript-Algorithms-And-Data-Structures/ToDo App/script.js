ba4c2ce475f171b47ee137500a90605fc46abbd7
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-12 18:01:41 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/ToDo App/script.js
+++ Javascript-Algorithms-And-Data-Structures/ToDo App/script.js
@@ -48,11 +48,20 @@ const updateTaskContainer = () => {
   );
 };
 
+
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+
+  buttonEl.parentElement.remove();
+  taskData.splice(dataArrIndex, 1);
+}
+
+const editTask = (buttonEl) => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
   
+
 }
 
 const reset = () => {
