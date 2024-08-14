295338c3e4e09dfea4932c9a45d944e00b58fb9d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-14 19:27:31 +0530

    ok

---
+++ Javascript-Algorithms-And-Data-Structures/Decimal to Binary/script.js
@@ -0,0 +1,19 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const checkUserInput = () => {
+  if (!numberInput.value) {
+    
+  }
+
+  console.log(numberInput.value);
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
