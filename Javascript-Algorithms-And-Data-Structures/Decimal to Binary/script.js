75a9a3ecc6ae5069b5b59d3067302772959503c8
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-16 19:45:27 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/Decimal to Binary/script.js
+++ Javascript-Algorithms-And-Data-Structures/Decimal to Binary/script.js
@@ -1,18 +1,4 @@
-const callStack = [];
 
-const a = () => {
-  return "freeCodeCamp " + b();
-};
-
-const b = () => {
-  return "is " + c();
-};
-
-const c = () => {
-  return "awesome!";
-};
-
-console.log(a());
 
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
