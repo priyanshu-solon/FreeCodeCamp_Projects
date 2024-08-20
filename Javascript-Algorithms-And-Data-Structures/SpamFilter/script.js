e5340f980587918baa6fdc87fbc2758341817068
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-20 18:31:43 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpamFilter/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpamFilter/script.js
@@ -2,7 +2,12 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /dollars/i;
+
+const denyList = [helpRegex, dollarRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
@@ -10,5 +15,8 @@ checkMessageButton.addEventListener("click", () => {
     return;
   }
 
-
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
 });
