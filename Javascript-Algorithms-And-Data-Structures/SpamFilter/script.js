bac4f3f43900bf97fed7da128204968e1d6c48c7
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-20 20:48:45 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpamFilter/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpamFilter/script.js
@@ -3,9 +3,12 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex = /dollars/i;
+const dollarRegex = /[0-9]+ (?:hundred|thousand|million|billion)? dollars/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /(?:^|\s)d[e3][a4@]r fr[i|1][e3]nd(?:$|\s)/i;
 
-const denyList = [helpRegex, dollarRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
 const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
