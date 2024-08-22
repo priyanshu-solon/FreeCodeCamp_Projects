6bc92d9d2c442bf726f3a7f8ddfedbd96cd809e8
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-22 23:00:11 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SortNumber/script.js
+++ Javascript-Algorithms-And-Data-Structures/SortNumber/script.js
@@ -1,5 +1,9 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
- event.preventDefault()
+  event.preventDefault();
+
+  const inputValues = document.getElementsByClassName("values-dropdown");
 }
+
+sortButton.addEventListener("click", sortInputArray);
