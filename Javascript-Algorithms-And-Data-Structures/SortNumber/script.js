0c9e0e3c4f045cbabec99169a21a4f9aec021dbf
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-23 16:56:36 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SortNumber/script.js
+++ Javascript-Algorithms-And-Data-Structures/SortNumber/script.js
@@ -7,9 +7,11 @@ const sortInputArray = (event) => {
     ...document.getElementsByClassName("values-dropdown")
   ].map((dropdown) => Number(dropdown.value));
 
+  const sortedValues = inputValues.sort((a, b) => {
+    return a - b;
+  });
 
-
-  updateUI(inputValues);
+  updateUI(sortedValues);
 }
 
 const updateUI = (array = []) => {
@@ -22,9 +24,47 @@ const updateUI = (array = []) => {
 const bubbleSort = (array) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - 1; j++) {
-      console.log(array, array[j], array[j + 1]);
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
   }
+
+  return array;
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
 }
 
 sortButton.addEventListener("click", sortInputArray);
