7a75338ad26d34a7a4fa13280517dbe0f5656a70
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-25 19:35:58 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
+++ Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
@@ -1,5 +1,10 @@
+const getMean = (array) => {
+  const sum = array.reduce((acc, el) => acc + el, 0);
+  const mean=sum/array.length;
+}
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el));
+  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 }
