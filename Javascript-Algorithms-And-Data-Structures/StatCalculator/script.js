bc23b647745593d7b4a293f0e5f1b1d01b38ebcc
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-26 19:39:40 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
+++ Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
@@ -2,8 +2,11 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 const testArr1 = [1, 2, 3, 4, 5];
 const testArr2 = [1, 2, 3, 4, 5, 6];
-const isEven = testArr2.length%2===0;
-console.log(isEven)
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
 }
