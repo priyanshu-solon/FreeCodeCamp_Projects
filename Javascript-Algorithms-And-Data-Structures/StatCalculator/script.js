a50e9fd74c1c6b63abffd95c003c46316248bdd7
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-29 22:57:54 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
+++ Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
@@ -1,27 +1,30 @@
-const getMean = array => array.reduce((acc, el) => acc + el, 0) / array.length;
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
-const getMedian = array => {
+const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
   const median =
     array.length % 2 === 0
       ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
       : sorted[Math.floor(array.length / 2)];
   return median;
-};
+}
 
-const getMode = array => {
+const getMode = (array) => {
   const counts = {};
 
-};
+  return array;
+}
 
 const calculate = () => {
-  const value = document.querySelector('#numbers').value;
+  const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  console.log(getMode(numbers));
 
-  document.querySelector('#mean').textContent = mean;
-  document.querySelector('#median').textContent = median;
-};
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+}
+
