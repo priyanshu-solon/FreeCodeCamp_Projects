5bdb17753067e6b9b6256dd18a5eb58e805ecf19
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-30 00:09:46 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
+++ Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
@@ -36,21 +36,28 @@ const getVariance = (array) => {
     const difference = el - mean;
     const squared = difference ** 2;
     return acc + squared;
-  }, 0);
+  }, 0) / array.length;
+  return variance;
+}
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
 }
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
   const range = getRange(numbers);
+  const variance = getStandardDeviation(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
   document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 }
