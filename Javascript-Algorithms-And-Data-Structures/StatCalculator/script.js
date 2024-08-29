330ec6ac8e02e6764b329fe061aeca99c34ac53d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-29 23:12:38 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
+++ Javascript-Algorithms-And-Data-Structures/StatCalculator/script.js
@@ -11,10 +11,13 @@ const getMedian = (array) => {
 
 const getMode = (array) => {
   const counts = {};
-  array.forEach(el => counts[el] = (counts[el] || 0) + 1)
-  if(new Set(Object.values(counts)).size===1){
-    return null
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
   }
+  const highest = Object.keys(counts);
 }
 
 
