f40c7957786a7210c267f2b14b0984436085dab2
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-08-30 11:44:05 +0530

    ok

---
+++ Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
@@ -0,0 +1,11 @@
+const range = (start, end) => Array(end - start + 1);
+
+window.onload = () => {
+  const container = document.getElementById("container");
+  const createLabel = (name) => {
+    const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
+  }
+}
