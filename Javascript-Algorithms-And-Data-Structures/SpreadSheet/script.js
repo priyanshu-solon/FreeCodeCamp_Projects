6361fb541f414140272bbb0849de23fa9b02e8f4
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-01 11:36:38 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
@@ -10,5 +10,15 @@ window.onload = () => {
     container.appendChild(label);
   }
   const letters = charRange("A", "J");
-
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+       
+    })
+  })
 }
