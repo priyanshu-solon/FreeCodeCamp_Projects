e0b7cf404b84db5705df0f95e4d5f5819d8c8387
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-03 16:43:24 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
@@ -2,8 +2,11 @@ const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 const average = nums => sum(nums) / nums.length;
 
-const median=(nums)=>{
+const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
