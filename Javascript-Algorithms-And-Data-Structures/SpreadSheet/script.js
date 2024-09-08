0c347727b17c8fc10f0891f648fc1a5cbe19cd98
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-08 11:38:49 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
@@ -24,8 +24,11 @@ const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
-  const elemValue = (num)=>{
-
+  const elemValue = num => {
+    const inner = character => {
+      return idToText(character+num)
+    }
+    return inner;
   }
 }
 
