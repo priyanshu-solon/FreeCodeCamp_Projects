89f5bd6dea30292bd00a4edfffea144836b0e01c
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-18 15:54:45 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
+++ Javascript-Algorithms-And-Data-Structures/SpreadSheet/script.js
@@ -5,7 +5,7 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 }
 
-const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => {});
+const infixEval = (str, regex) => str.replace(regex, (_match, arg1, operator, arg2) => infixToFunction[operator]);
 
 const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
