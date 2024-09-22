407462cac23f9a9b6ee05f46526e03491a674d1f
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-22 22:09:37 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -86,5 +86,11 @@ const products = [
 ];
 
 products.forEach(
-  ({ name, id, price, category }) => {}
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+    <div class="dessert-card">
+    <h2>${name}</h2>
+    </div>
+    `;
+  }
 );
