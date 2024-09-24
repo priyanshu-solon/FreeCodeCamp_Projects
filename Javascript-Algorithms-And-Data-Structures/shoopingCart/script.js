b7c6ac279f1dbfc10c474047cfd03fe8d34f7adc
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-24 18:38:59 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -85,12 +85,15 @@ const products = [
   },
 ];
 
-products.forEach(({ name, id, price, category }) => {
-  dessertCards.innerHTML += `
-    <div class="dessert-card">
-      <h2>${name}</h2>
-      <p class="dessert-price">$${price}</p>
-      <p class="product-category">Category: ${category}</p>
-    </div>
-  `;
-});
+products.forEach(
+  ({ name, id, price, category }) => {
+    dessertCards.innerHTML += `
+      <div class="dessert-card">
+        <h2>${name}</h2>
+        <p class="dessert-price">$${price}</p>
+        <p class="product-category">Category: ${category}</p>
+        <button id=${id} class="btn add-to-cart-btn">Add to cart</button>
+      </div>
+    `;
+  }
+);
