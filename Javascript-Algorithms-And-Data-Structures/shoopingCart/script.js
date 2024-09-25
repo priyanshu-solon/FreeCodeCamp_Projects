6589dfad7c554ae32043b98704fc254f907e0f6d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-25 19:34:10 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -112,6 +112,10 @@ class ShoppingCart {
     const product = products.find((item) => item.id === id);
     const { name, price } = product;
     this.items.push(product);
-    const totalCountPerProduct={}
+
+    const totalCountPerProduct = {};
+    this.items.forEach((dessert)=>{
+
+    })
   }
 };
