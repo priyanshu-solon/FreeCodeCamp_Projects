60a6762ac64e3636737a5812b391a770006e3f5e
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-28 18:42:39 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -121,6 +121,8 @@ class ShoppingCart {
     const currentProductCount = totalCountPerProduct[product.id];
     const currentProductCountSpan = document.getElementById(`product-count-for-id${id}`);
 
-    currentProductCount > 1 ? currentProductCountSpan.textContent  = `${currentProductCount}x`: undefined ;
+    currentProductCount > 1 
+      ? currentProductCountSpan.textContent = `${currentProductCount}x`
+      : productsContainer.innerHTML += `<div class="product" id="dessert${id}"></div>`;
   }
 };
