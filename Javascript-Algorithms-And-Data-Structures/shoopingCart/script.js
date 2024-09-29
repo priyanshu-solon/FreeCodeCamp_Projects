e173e3a75420c5e7ed9e47a24bc705e89f3b298e
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-09-29 16:13:21 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -125,9 +125,14 @@ class ShoppingCart {
       ? currentProductCountSpan.textContent = `${currentProductCount}x`
       : productsContainer.innerHTML += `
       <div id="dessert${id}" class="product">
-      <p>price</p>
-      <p>${price}</p>
+        <p>
+          <span class="product-count" id="product-count-for-id${id}"></span>${name}
+        </p>
+        <p>${price}</p>
       </div>
       `;
   }
 };
+
+const cart = new ShoppingCart();
+const addToCartBtns= document.getElementsByClassName("add-to-cart-btn")
