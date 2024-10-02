a4db288a38a39fabc6adad2e4c730c3ffdb22f35
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-10-02 20:48:06 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -138,7 +138,9 @@ const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
 
 [...addToCartBtns].forEach(
-(btn)=>{
-  btn.addEventListener('click',(event)=>{})
-}
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+cart.addItem(Number(event.target.id),products)
+    })
+  }
 );
