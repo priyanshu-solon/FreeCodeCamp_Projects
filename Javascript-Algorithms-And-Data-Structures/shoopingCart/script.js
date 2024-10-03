7354849b3b92e036f6943598ae18ac471f7b8a61
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-10-03 23:29:12 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
+++ Javascript-Algorithms-And-Data-Structures/shoopingCart/script.js
@@ -137,6 +137,27 @@ class ShoppingCart {
     return this.items.length;
   }
 
+  clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+    if (isCartCleared) {
+      this.items = [];
+      this.total = 0;
+      productsContainer.innerHTML = "";
+      totalNumberOfItems.textContent = 0;
+      cartSubTotal.textContent = 0;
+      cartTaxes.textContent = 0;
+      cartTotal.textContent = 0;
+    }
+  }
+
   calculateTaxes(amount) {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
@@ -145,6 +166,10 @@ class ShoppingCart {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
     const tax = this.calculateTaxes(subTotal);
     this.total = subTotal + tax;
+    cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
   }
 };
 
@@ -156,6 +181,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
       totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
     })
   }
 );
@@ -165,3 +191,5 @@ cartBtn.addEventListener("click", () => {
   showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
   cartContainer.style.display = isCartShowing ? "block" : "none";
 });
+
+clearCartBtn.addEventListener("click",cart.clearCart.bind(cart))
