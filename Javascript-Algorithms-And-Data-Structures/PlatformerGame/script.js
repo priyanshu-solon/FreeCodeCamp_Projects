1a2116ff94a721dab5c7db6f4fd66c8b3e1c0e92
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-10-06 13:06:15 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/PlatformerGame/script.js
+++ Javascript-Algorithms-And-Data-Structures/PlatformerGame/script.js
@@ -1,2 +1,7 @@
-const startBtn = document.getElementById("start-btn")
-const canvas = document.getElementById("canvas")
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p");
+
+
