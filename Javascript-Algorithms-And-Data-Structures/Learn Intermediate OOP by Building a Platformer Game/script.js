76f9ac9f9305bc82f10b962abea0aea700dff69d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-10-23 19:10:14 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -73,23 +73,22 @@ class Platform {
 
 const player = new Player();
 
-
 const platformPositions = [
   { x: 500, y: proportionalSize(450) },
   { x: 700, y: proportionalSize(400) },
-  { x:850 , y: proportionalSize(350) },
-  { x:900 , y: proportionalSize(350) },
-  { x:1050, y: proportionalSize(150) },
-  { x:2500, y: proportionalSize(450) },
-  { x:2900, y: proportionalSize(400) },
-  { x:3150, y: proportionalSize(350) },
-  { x:3900, y: proportionalSize(450) },
-  { x:4200, y: proportionalSize(400) },
-  { x:4400, y: proportionalSize(200) },
-  { x:4700, y: proportionalSize(150) },
-
+  { x: 850, y: proportionalSize(350) },
+  { x: 900, y: proportionalSize(350) },
+  { x: 1050, y: proportionalSize(150) },
+  { x: 2500, y: proportionalSize(450) },
+  { x: 2900, y: proportionalSize(400) },
+  { x: 3150, y: proportionalSize(350) },
+  { x: 3900, y: proportionalSize(450) },
+  { x: 4200, y: proportionalSize(400) },
+  { x: 4400, y: proportionalSize(200) },
+  { x: 4700, y: proportionalSize(150) },
 ];
 
+const platforms=platformPositions.map()
 
 const animate = () => {
   requestAnimationFrame(animate);
