d7e01ba2f236ffceaaf95af0d2cc24a7e5bbe4ab
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-10-30 13:11:21 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -120,15 +120,17 @@ const animate = () => {
     }
   }
 
-
   platforms.forEach((platform) => {
     const collisionDetectionRules = [
       player.position.y + player.height <= platform.position.y,
       player.position.y + player.height + player.velocity.y >= platform.position.y,
       player.position.x >= platform.position.x - player.width / 2,
-      player.position.x <= platform.position.x + platform.width - player.width / 3
-
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
     ];
+
+if (collisionDetectionRules.every(rule => rule)) { player.velocity.y = 0; return; 
+}
   });
 
 }
