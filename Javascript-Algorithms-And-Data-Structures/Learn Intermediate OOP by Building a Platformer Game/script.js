eaa4eab62d3f317d271accbec6c442a255836bb6
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-11-18 21:17:04 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -80,8 +80,14 @@ class CheckPoint {
     };
     this.width = proportionalSize(40);
     this.height = proportionalSize(70);
-this.claimed = false;
+    this.claimed = false;
   };
+
+  draw() {
+    ctx.fillStyle = "#f1be32";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
 };
 
 
