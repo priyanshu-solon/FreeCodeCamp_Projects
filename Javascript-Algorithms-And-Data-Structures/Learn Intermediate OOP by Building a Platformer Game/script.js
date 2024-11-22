7b2b5e668cf7e8e7bf7d931f3e2b3faea48808a7
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-11-22 19:31:23 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn Intermediate OOP by Building a Platformer Game/script.js
@@ -194,17 +194,31 @@ const animate = () => {
     };
   });
 
-
   checkpoints.forEach((checkpoint, index, checkpoints) => {
-    const checkpointDetectionRules =[
+    const checkpointDetectionRules = [
       player.position.x >= checkpoint.position.x,
       player.position.y >= checkpoint.position.y,
       player.position.y + player.height <=
         checkpoint.position.y + checkpoint.height,
       isCheckpointCollisionDetectionActive,
-    ]
-  });
+      player.position.x - player.width <=
+        checkpoint.position.x - checkpoint.width + player.width * 0.9,
+      index === 0 || checkpoints[index - 1].claimed === true,
+    ];
 
+    if (checkpointDetectionRules.every((rule) => rule)) {
+      checkpoint.claim();
+
+
+      if (index === checkpoints.length - 1) {
+        isCheckpointCollisionDetectionActive = false;
+        showCheckpointScreen("You reached the final checkpoint!")
+        movePlayer("ArrowRight",0,false)
+      };
+
+
+    };
+  });
 }
 
 
