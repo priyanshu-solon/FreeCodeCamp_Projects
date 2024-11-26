5ce6b80c2ad5ee5a5086aae1a233e26e1517778c
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-11-26 22:06:17 +0530

    ok

---
+++ Javascript-Algorithms-And-Data-Structures/Review algorithmic thinking by Building a dice game/script.js
@@ -0,0 +1,18 @@
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const roundElement = document.getElementById("current-round");
+const rollsElement = document.getElementById("current-round-rolls");
+const totalScoreElement = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
+
+let diceValuesArr = [];
+let isModalShowing = false;
+let score = 0;
+let round = 1; 
+let rolls = 0; 
+
