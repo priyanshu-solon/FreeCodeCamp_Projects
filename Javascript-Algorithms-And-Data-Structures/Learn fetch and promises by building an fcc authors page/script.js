db40d35bd35a4015ecea17be5dff202abb053a7d
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-12-11 18:20:31 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn fetch and promises by building an fcc authors page/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn fetch and promises by building an fcc authors page/script.js
@@ -2,4 +2,7 @@ const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
-.then((res) => res.json())
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
