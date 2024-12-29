cdaafb3657997887131be5364556890a6530e31c
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-12-29 18:31:31 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -1,2 +1,4 @@
-const forumLatest="https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
-const forumTopicUrl="https://forum.freecodecamp.org/t/"
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl ="https://forum.freecodecamp.org/c/"
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp"
