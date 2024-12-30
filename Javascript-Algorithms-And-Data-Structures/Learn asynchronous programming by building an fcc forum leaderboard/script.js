bd0e3b77b72c8175dba2f7fed25fc82904f50f2a
Priyanshu Solon <priyanshusolon2001@gmail.com>
2024-12-30 22:45:17 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -2,4 +2,8 @@ const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
-const postsContainer = document.getElementById("posts-container")
+
+const postsContainer = document.getElementById("posts-container");
+const fetchData = async () =>{
+  
+}
