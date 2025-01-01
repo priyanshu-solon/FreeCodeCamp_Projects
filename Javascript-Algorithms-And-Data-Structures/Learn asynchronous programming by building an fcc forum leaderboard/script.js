d4f029242d8cdfd806950a3b86d07fada461b5a3
Priyanshu Solon <priyanshusolon2001@gmail.com>
2025-01-01 20:31:34 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -9,9 +9,9 @@ const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
-    console.log(data);
   } catch (err) {
-    
+    console.log(err);
   }
 };
-fetchData()
+
+fetchData();
