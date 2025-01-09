44dc2f048da013dc0bfd262dcce23beff58a02d6
Priyanshu Solon <priyanshusolon2001@gmail.com>
2025-01-09 21:26:21 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -5,6 +5,11 @@ const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+function timeAgo(time){
+  const currentTime = new Date()
+  const lastPost = new Date(time)
+}
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
@@ -28,8 +33,7 @@ const showLatestPosts = (data) => {
       views,
       posts_count,
       slug,
-      posters,
-      category_id,
+      posters,_id,
       bumped_at,
     } = item;
 
@@ -40,9 +44,7 @@ const showLatestPosts = (data) => {
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>
-      ${views}
-      </td>
+      <td>${views}</td>
       <td></td>
     </tr>`;
   }).join("");
