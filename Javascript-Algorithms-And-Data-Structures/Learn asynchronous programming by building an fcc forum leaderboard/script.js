ee6aeca01ae890bff46e27520aa3b49763edf260
Priyanshu Solon <priyanshusolon2001@gmail.com>
2025-01-24 17:37:33 +0530

    ok

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -30,7 +30,7 @@ const forumCategory = (id) => {
     selectedCategory.id = 1;
   }
   const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
-
+const linkText = selectedCategory.category
 };
 
 const timeAgo = (time) => {
