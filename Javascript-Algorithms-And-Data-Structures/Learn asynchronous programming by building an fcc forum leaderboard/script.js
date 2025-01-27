a995d4fd92f408c9a5123796b5057cda3172aad0
Priyanshu Solon <priyanshusolon2001@gmail.com>
2025-01-27 19:39:16 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -120,8 +120,9 @@ const showLatestPosts = (data) => {
         ${forumCategory(category_id)}
       </td>
       <td>
-<div class="avatar-container">
-</div>
+        <div class="avatar-container">
+        ${avatars(posters,users)}
+        </div>
       </td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
