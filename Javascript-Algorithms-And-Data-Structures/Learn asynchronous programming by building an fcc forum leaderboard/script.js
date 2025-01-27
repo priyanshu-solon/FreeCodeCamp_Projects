5ada1cdad2f8d44250ece7c4178a02d8f2393e8e
Priyanshu Solon <priyanshusolon2001@gmail.com>
2025-01-27 19:35:27 +0530

    Update script.js

--- Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
+++ Javascript-Algorithms-And-Data-Structures/Learn asynchronous programming by building an fcc forum leaderboard/script.js
@@ -75,9 +75,10 @@ const avatars = (posters, users) => {
     const user = users.find((user) => user.id === poster.user_id);
     if (user) {
       const avatar = user.avatar_template.replace(/{size}/, 30);
-const userAvatarUrl = avatar.startsWith("/user_avatar/")
+      const userAvatarUrl = avatar.startsWith("/user_avatar/")
         ? avatarUrl.concat(avatar)
         : avatar;
+      return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
     }
   });
 };
@@ -123,3 +124,4 @@ const showLatestPosts = (data) => {
     </tr>`;
   }).join("");
 };
+
